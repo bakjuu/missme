f59f0b6f2a7930fe9e3a8f44b2ddf1ba255bbdce
jypark <jypark@snghair.com>
2024-02-16 13:26:06 -0800

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,33 @@
 import { useState } from "react";
 import "./App.css";
-import IMG_1 from "./images/IMG_1.jpeg";
-import IMG_2 from "./images/IMG_2.JPG";
-import IMG_3 from "./images/IMG_3.PNG";
-import IMG_4 from "./images/IMG_4.JPG";
-import IMG_5 from "./images/IMG_5.JPG";
-import IMG_6 from "./images/IMG_6.JPG";
-import IMG_7 from "./images/IMG_7.JPG";
-import IMG_8 from "./images/IMG_8.JPG";
-import IMG_9 from "./images/IMG_9.jpeg";
-import IMG_10 from "./images/IMG_10.JPG";
-import IMG_11 from "./images/IMG_11.JPG";
-import Final from "./images/myvalentine.gif";
-const images = [
-  IMG_1,
-  IMG_2,
-  IMG_3,
-  IMG_4,
-  IMG_5,
-  IMG_6,
-  IMG_7,
-  IMG_8,
-  IMG_9,
-  IMG_10,
-  IMG_11,
-];
+import IMG_1 from "./images/IMG_1.gif";
+import Final from "./images/Final.gif";
+// const images = [
+//   IMG_1,
+//   IMG_2,
+//   IMG_3,
+//   IMG_4,
+//   IMG_5,
+//   IMG_6,
+//   IMG_7,
+//   IMG_8,
+//   IMG_9,
+//   IMG_10,
+//   IMG_11,
+// ];
 
 const phrases = [
   "No 🙉",
   "진심이야?",
   "이럴거야?",
   "Really Sure😢",
-  "I'll be very sad😕",
+  "보고싶잖아..😕",
   "이러지마.. Please🥺",
-  "Don't do this to me🫤",
+  "다시 한번 생각해봐🫤",
   "I'm gonna cry..😭",
   "진심이 아니잖아..😭",
   "You are breaking my heart😭💔",
-  "마이 묵었따..😭💔",
+  "난 넌무 보고잡다..😭💔",
 ];
 
 function App() {
@@ -64,15 +54,15 @@ function App() {
           <div className="image-container">
             <img src={Final} alt="bear-Kissing" />
           </div>
-          <div className="text">B, My Valentine!</div>
+          <div className="text">B, I miss U ALREADY.. A LOT</div>
         </>
       ) : (
         <>
           <div className="image-container">
-            <img src={randomImage} alt="Randomized Heart" />
+            <img src={IMG_1} alt="Randomized Heart" />
           </div>
 
-          <div className="text">Will you be my forever valentine 🌹?</div>
+          <div className="text">Do you miss me already? 🌹?</div>
           <div className="both-Button">
             <button
               className="yesButton"
